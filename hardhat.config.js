16bd0e05a586487f8fba8e84c6ea171162d858a8
Ishaan <ishaanmaths@gmail.com>
2025-04-09 12:56:14 +0530

    Updated hardhat config file, installed dotenv modules

--- hardhat.config.js
+++ hardhat.config.js
@@ -2,11 +2,20 @@ require("@nomicfoundation/hardhat-toolbox");
 require("dotenv").config();
 
 module.exports = {
-  solidity: "0.8.19",
+  solidity: {
+    compilers: [
+      {
+        version: "0.8.28"
+      },
+      {
+        version: "0.8.20"
+      }
+    ]
+  },
   networks: {
     sepolia: {
-      url: process.env.SEPOLIA_RPC_URL, // RPC URL dalni from alchmy 
-      accounts: [process.env.PRIVATE_KEY], // wallet ki private key (sensitive)
-    },
-  },
+      url: process.env.SEPOLIA_RPC_URL || "", // Infura or Alchemy URL
+      accounts: process.env.PRIVATE_KEY ? [process.env.PRIVATE_KEY] : []
+    }
+  }
 };

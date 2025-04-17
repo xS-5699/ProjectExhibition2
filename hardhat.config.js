a0db98e1aa2306f7dd163ff79032e00fc622e2ca
Ishaan <ishaanmaths@gmail.com>
2025-04-18 04:04:49 +0530

    added frontend packages to root package.json for smoother modules installation.

--- hardhat.config.js
+++ hardhat.config.js
@@ -4,17 +4,13 @@ require("dotenv").config();
 module.exports = {
   solidity: {
     compilers: [
-      {
-        version: "0.8.28"
-      },
-      {
-        version: "0.8.20"
-      }
+      { version: "0.8.28" },
+      { version: "0.8.20" }
     ]
   },
   networks: {
     sepolia: {
-      url: process.env.SEPOLIA_RPC_URL || "https://eth-sepolia.g.alchemy.com/v2/cI95BuEnbzoSfyCcZo1v7P36Ar8BJnUo",
+      url: process.env.SEPOLIA_RPC_URL, // defined in .env
       accounts: process.env.PRIVATE_KEY ? [process.env.PRIVATE_KEY] : []
     }
   }
